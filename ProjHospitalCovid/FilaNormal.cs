a5c35356a74fc3cab062345e21bfab2f3ff543d9
Willian Proni <wifepro@hotmail.com>
2022-03-03 11:12:38 -0300

    Criação da class pacinte e divisão das filas

---
+++ ProjHospitalCovid/FilaNormal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjHospitalCovid
+{
+    internal class FilaNormal
+    {
+        public Paciente Head { get; set; }
+        public Paciente Tail { get; set; }
+
+        public FilaNormal()
+        {
+            Head = Tail = null;
+        }
+
+        public bool FilaNormalVazia()
+        {
+            if (Head == null && Tail == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void InserirPacienteFilaNormal(Paciente novopreferencial)
+        {
+            if (FilaNormalVazia())
+            {
+                Head = novopreferencial;
+                Tail = novopreferencial;
+            }
+            else
+            {
+                Tail.Proximo = novopreferencial;
+                Tail = novopreferencial;
+            }
+        }
+    }
+}
