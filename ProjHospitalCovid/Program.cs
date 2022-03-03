abbc61a72685a714a102d5c27cf47cf7378d83d5
Willian Proni <wifepro@hotmail.com>
2022-03-03 09:09:06 -0300

    Adicionar arquivos de projeto.

---
+++ ProjHospitalCovid/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ProjHospitalCovid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao;
+            Servicos servicos = new Servicos();
+            do
+            {
+                Menu();
+                opcao = int.Parse(Console.ReadLine());  
+                switch (opcao)
+                {
+                    case 0:
+                        Console.WriteLine("Fechando...");
+                        break;
+                    case 1:
+                        servicos.CadastrarPaciente();
+                        break;  
+                    default:
+                        Console.WriteLine("Opção Inválida!");
+                        break;
+                }
+            } while (opcao != 0);
+        }
+        public static void Menu()
+        {
+            Console.WriteLine("\n[1] - Cadastrar Paciente" +
+                              "\n[2] - Buscar Cadastro" +
+                              "\n[3] - Chamar para Triagem" +
+                              "\n[4] - Alta em Paciente" +
+                              "\n[0] - Fechar Sistema");
+        }
+    }
+}
---
+++ ProjHospitalCovid/Servicos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjHospitalCovid
+{
+    internal class Servicos
+    {
+        public void CadastrarPaciente()
+        {
+            Console.WriteLine("Nome do Paciente");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Qual o Sexo do paciente ");
+            char sexo = char.Parse(Console.ReadLine());
+            Console.WriteLine("Data de Nascimento: ");
+            DateTime dataNascimento = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Cpf do Paciente: ");
+            string cpf = Console.ReadLine();
+        }
+    }
+}
